384d5d635e0ed968e4736b404905ca63cb9e08f3
Iino Ten <nteten0313@icloud.com>
2020-11-02 01:06:22 +0900

    ツイート内容を元に記事を作成

--- create_article.py
+++ create_article.py
@@ -1,3 +1,9 @@
-def create_new_article( title, content ):
+def create_new_article( number, title, content ):
+  breaked_content = content.replace('\n\n','<br><br>')
+  file = open( './articles/'+str(number)+'.html', 'w' )
+  file.write(f"""
+  <h1>{title}</h1>
+  <h4>{breaked_content}</h4>
+  """)
   print("タイトル", title)
   print("本文", content)
