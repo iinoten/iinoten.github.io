a6c984f961b8a84690e5babc9791069613d430eb
Iino Ten <nteten0313@icloud.com>
2020-11-03 14:27:17 +0900

    スタイルシートの追加

--- create_article.py
+++ create_article.py
@@ -25,7 +25,7 @@ def create_new_article( number, title, content ):
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="test.css">
+        <link rel="stylesheet" href="article.css">
         <title>Document</title>
         <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
     </head>
