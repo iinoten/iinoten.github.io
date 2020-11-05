ffbd83a25e379203c93a0e0b34bd8d977a3ac6da
Iino Ten <nteten0313@icloud.com>
2020-11-05 14:04:47 +0900

    "ブログ記事をCommit"

--- about_index.py
+++ about_index.py
@@ -1,4 +1,27 @@
 def set_new_article_to_index( title, date, file_name ):
+  init_index_file = open('index.html','w')
+  init_index_file.write(
+    '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="index.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="blog__description--Box">
+            <div class="blog__description--text">このブログは、Twitterで特定のハッシュタグに反応して、botが内容を取得しブログ形式でまとめられています。</div>
+        </div>
+        <div class="title__box">
+            <div class="title__text">記事一覧</div>
+        </div>
+        <div id="empty__element"></div>
+    </body>
+    </html>
+    '''
+  )
   file = open('index.html', 'r' )
   index_text = file.read()
   replaced_index_text = index_text.replace('<div id="empty__element"></div>', f'<div id="empty__element"></div>\n   <a href="./articles/{file_name}.html" class="article__box"><div class="article__title">{title}</div><div class="article__date">{date}</div></a>\n')
